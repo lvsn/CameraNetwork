fa50a8c39ec646c3c6c7535c64e96b152e8b58a2
Mathieu Garon <mathieugaron1991@hotmail.com>
2014-05-23 16:31:07 -0400

    add ip parameter in server at launch time

--- src/camera_controler/scripts/ip_sender.py
+++ src/camera_controler/scripts/ip_sender.py
@@ -7,7 +7,7 @@ from std_msgs.msg import String
 def ip_sender():
     pub = rospy.Publisher('ipchat', String, queue_size=10)
     rospy.init_node('ip_sender', anonymous=True)
-    r = rospy.Rate(0.5) # 1hz
+    r = rospy.Rate(0.1) # 1hz
     while not rospy.is_shutdown():
         str = os.environ.get('ROS_IP')
         #rospy.loginfo(str)
