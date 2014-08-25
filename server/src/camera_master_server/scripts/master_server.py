ab132b995578dedbc6a51cd0029ec6f292d4860e
yahog <YAHOG@havreaubert.gel.ulaval.ca>
2014-08-25 11:44:16 -0400

    Added customization of the Picture path

--- server/src/camera_master_server/scripts/master_server.py
+++ server/src/camera_master_server/scripts/master_server.py
@@ -8,29 +8,53 @@ Created on Mon Jun  9 14:41:12 2014
 
 Server that handle action/services from user.
 """
+from __future__ import print_function
+
+import os
+import errno
 
 import roslib; roslib.load_manifest('camera_master_server')
 import rospy
+
 import file_transfer_action as fta
 import network_capture_action as nca
-import os
 
-class server:
-    
+
+class Server:
     def __init__(self):
         rospy.init_node('master_server')
-        self.imagePath = "/home/CameraNetwork"
-        if not os.path.exists(self.imagePath):
-            rospy.logfatal("Install file not executed! : CameraNetwork path not set")
+        self.setPictureDirectory()
         self.sftp = fta.sftp_action(self.imagePath)
         self.network_capture = nca.network_capture_action()
         rospy.on_shutdown(self.shutdown)
         rospy.spin()
 
+    def setPictureDirectory(self):
+        """
+        Sets the self.imagePath variable according to CAMNET_OUTPUT_DIR
+        environment variable. If not found, reverts to ~/Pictures.
+        If the directory does not exists, it tries to create it.
+        """
+        try:
+            self.imagePath = os.environ["CAMNET_OUTPUT_DIR"]
+        except KeyError:
+            self.imagePath = os.path.expanduser("~/Pictures")
+            rospy.logwarn(
+                "Could not find the CAMNET_OUTPUT_DIR environment variable.\n"
+                "Reverting to ~/Pictures as the picture directory."
+            )
+
+        try:
+            os.makedirs(self.imagePath)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                rospy.logfatal("Install file not executed! : "
+                               "CameraNetwork path not set")
+
     def shutdown(self):
         del self.sftp
         del self.network_capture
 
+
 if __name__ == "__main__":
-    
-    serverInstance = server()
+    serverInstance = Server()
