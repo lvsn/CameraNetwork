fd4949c6a6cac8545908df50332ff3f37052d147
jbecirovski <julien@becirovski.com>
2015-07-17 12:05:36 -0400

    bugfix server installer path file

--- server/install_server.sh
+++ server/install_server.sh
@@ -4,16 +4,18 @@
 # Install Camera-Network Server #
 #################################
 
+ROS_SERVER_PATH=/home-local/yahog.extra.nobkp/CameraNetwork
+
 echo '*** CAMNET SERVER INSTALLATION BEGIN ***'
 set +e
-cd ~/camera-network/server/
+cd $ROS_SERVER_PATH/server/
 echo '*** CATKIN_MAKE ***'
 catkin_make
 echo '*** CATKIN_MAKE INSTALL ***'
 catkin_make install
 echo '*** INSTALL CUSTOM FILES ***'
-ln -sv ~/camera-network/server/src/camera_master_server/launch ~/camera-network/server/install/share/camera_master_server/
-ln -sv ~/camera-network/server/src/camera_master_server/scripts/*.py ~/camera-network/server/install/share/camera_master_server/
+ln -sv $ROS_SERVER_PATH/server/src/camera_master_server/launch $ROS_SERVER_PATH/server/install/share/camera_master_server/
+ln -sv $ROS_SERVER_PATH/server/src/camera_master_server/scripts/*.py $ROS_SERVER_PATH/server/install/share/camera_master_server/
 set -e
 echo '*** CAMNET SERVER INSTALLATION DONE ***'
 
