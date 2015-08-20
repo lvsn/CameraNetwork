3672422485957bda722bc1a8457688b9dadd2261
jbecirovski <julien@becirovski.com>
2015-08-20 07:46:48 -0400

    modify startup client env var

--- client/start_camnet.sh
+++ client/start_camnet.sh
@@ -2,12 +2,12 @@
 
 export ROS_IP=$(/sbin/ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 export ROS_MASTER_URI=http://victoria.gel.ulaval.ca:11311
-#export CAMNET_OUTPUT_DIR=/home
-export CAMNET_SERVER_DATA_DIR=~/Pictures
-#export CAMNET_SERVER_USER=
+export CAMNET_CAM_NAME=unSpecCam
+CAMNET_OUTPUT_DIR=/home/pi/camera-output/*
+export CAMNET_SERVER_DATA_DIR=JUBEC7@victoria.gel.ulaval.ca:/home-local/yahog.extra.nobkp/www/pictures/unprocessed/
 
 /bin/ps aux | /bin/grep roslaunch | /bin/grep camera_controler_gphoto.launch &> /dev/null
 if [ $? -ne 0 ]; then
     source ~/camera-network/client/install/setup.bash
-    CAMERA_NAME=SkyCam roslaunch ~/camera-network/client/src/camera_controler/launch/camera_controler_gphoto.launch
+    CAMERA_NAME=$CAMNET_CAM_NAME roslaunch ~/camera-network/client/src/camera_controler/launch/camera_controler_gphoto.launch
 fi
