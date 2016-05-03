c1da0fcc9f075c66718e5dce1d4d9d49c6f2f53d
Dominic <dbil25@hotmail.com>
2016-05-03 10:43:28 -0400

    Add ROS_IP

--- server/launch.sh
+++ server/launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ./install/setup.bash
-roslaunch ~/camera-network/server/install/share/camera_master_server/launch/camera_master_server.launch
+ROS_IP=$(/sbin/ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/') roslaunch /home-local/yahog.extra.nobkp/CameraNetwork/server/install/share/camera_master_server/launch/camera_master_server.launch
