51be6d024804e3ac1c186866c3b805103416ec62
Dominic <dbil25@hotmail.com>
2016-08-04 16:59:34 -0400

    server changes

--- server/src/camera_master_server/scripts/theta_timelapse_action.py
+++ server/src/camera_master_server/scripts/theta_timelapse_action.py
@@ -19,7 +19,6 @@ from scripts.Util.miscellaneous import *
 class theta_timelapse_action:
     def __init__(self):
         rospy.loginfo("Initialising Theta timlapse action")
-        self.capturePublisher = rospy.Publisher("/theta_capture", ThetaCapture, queue_size=1)
         self.optionsPublisher = rospy.Publisher("/theta_options", ThetaOptions, queue_size=1)
         rospy.sleep(0.5)    # let time for connections
         self.action = actionlib.SimpleActionServer('theta_timelapse', ThetaControlAction,
@@ -27,7 +26,6 @@ class theta_timelapse_action:
         self.action.start()
         
         self.picture_count = 0
-        self.captureMsg = ThetaCapture()
         self.optionsMsg = ThetaOptions()
         
     def execute(self, goal):
@@ -35,66 +33,9 @@ class theta_timelapse_action:
         self.optionsMsg.fileformat = goal.fileformat
         self.optionsMsg.shutterspeed = goal.shutterspeed
         self.optionsPublisher.publish(self.optionsMsg)
-        
-        self.captureMsg.capture = True
 
-        period = goal.period
-        hz = self._sec_to_hz(period)
-        #picture_goal = self._get_frame_qty(goal.picture_qty)
-        self.picture_count = 0
 
-        #while self.picture_count < picture_goal:
-        while 1:
-            timestamp = rospy.get_time() + period
-            self.picture_count += 1
 
-            self.capturePublisher.publish(self.captureMsg)
-
-            self._send_feedback(self.picture_count, 10000000, hz)
-            interupt = self._sleep(timestamp)
-            if interupt:
-                break
-        #succes_msg = CameraControlActionResult
-        #succes_msg.total_picture = 'Total Picture : ' + str(self.picture_count)
-        #self.action.set_succeeded(succes_msg)
-
-    def _sleep(self, timestamp):
-        """
-        Sleep until the timestamp (in seconds) if there is an interuption to the action, it return True.
-        it give a resolution of 2 sec.
-        :param timestamp:
-        :return bool:
-        """
-        while timestamp > rospy.get_time():
-                rospy.sleep(2)
-                if self.action.is_preempt_requested(
-                ) or not self.action.is_active():
-                    return True
-        return False
-
-    @staticmethod
-    def _sec_to_hz(Tsec):
-        try:
-            hz = math.fabs(1/Tsec)
-            rospy.loginfo("Frequency set to " + str(hz) + " hz.")
-        except ZeroDivisionError:
-            hz = -1
-            rospy.logwarn("Can not set 0 as frequency... setting frequency to 1 hz")
-        return hz
-
-    @staticmethod
-    def _get_frame_qty(Qty):
-        if Qty < 0:
-            frame_qty = float('inf')
-        else:
-            frame_qty = Qty
-        return frame_qty
-            
-    def _send_feedback(self, count, goal, frequency):
-        feedback_msg = CameraControlActionFeedback
-        feedback_msg.picture_taken = 'Picture taken:' + str(count) \
-            + '/' + str(goal) + ' (' + str(frequency) + 'Hz)'
-        self.action.publish_feedback(feedback_msg)
 
 if __name__ == '__main__':
     # Initialize the node and name it.
